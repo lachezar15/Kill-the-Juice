e74b20a994f078f6e0e05ddf3636fa9b94403140
simoto08 <ivanovsimeon690@gmail.com>
2025-10-02 21:10:18 +0300

    Main Menu

---
+++ Kill the Juice/Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
